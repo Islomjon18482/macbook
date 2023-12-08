1fda30801a7512ccdbcd779a35b32026e1c29df1
Islomjon Abduganiyev <islomjonabduganiyev@Islomjons-MacBook-Air.local>
2023-12-08 09:58:26 +0500

    project complated

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1-masala
+
+// function sum(arg1){
+//     return arg1**3
+// }
+// let a = +prompt("Ixtiyoriy son kiriting")
+// alert(sum(a));
+
+// 2-masala
+
+function sum(arg1, arg2){
+    let res = (arg1+arg2)**2
+    return res
+}
+let a = +prompt("Ixtiyoriy son kiriting")
+let b = +prompt("Ixtiyoriy son kiriting")
+alert(sum(a, b))
